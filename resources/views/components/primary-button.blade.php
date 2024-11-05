3b5d065d5f489e24e85e9bf3f2a85ac8bf2cdd6d
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2024-11-06 05:45:52 +0700

    update view login

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-10 text-center flex justify-center w-full py-4 border border-white rounded-md font-semibold text-lg text-white uppercase tracking-widest hover:bg-white hover:bg-opacity-20 focus:bg-white focus:bg-opacity-20 active:bg-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
