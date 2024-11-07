6c694f169a6536d24bc15de100c8d41dbfba97af
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2024-11-07 11:13:40 +0700

    update

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,20 +1,20 @@
 <x-app-layout>
     <div class="">
         <div class="pl-6 lg:pl-8 space-y-6">
-            <h1 class="text-white text-3xl">Edit Profile</h1>
-            <div class="p-4 sm:p-8 bg-white bg-opacity-20 shadow sm:rounded-lg">
+            <h1 class="text-white text-xl font-bold">Edit Profile</h1>
+            <div class="p-4 sm:p-8 bg-white rounded-xl bg-opacity-20 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white bg-opacity-20 shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 rounded-xl bg-white bg-opacity-20 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white bg-opacity-20 shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 rounded-xl bg-white bg-opacity-20 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
